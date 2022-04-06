a763afbb1e7c0617fa3685aa5d0550a43324be01
José Molina <jose.molina@criptan.com>
2022-04-06 13:44:49 +0200

    Add extra arguments and handle relative paths

--- bin/index.js
+++ bin/index.js
@@ -5,16 +5,27 @@
 const path = require('path')
 const fs = require('fs-extra')
 const coveralls = require('../')
+const yargs = require('yargs')
+
+const argv = yargs
+  .option('coverage-dir', {
+    description: 'Specify where the coverage directory is inside each repository',
+    default: 'coverage',
+    demandOption: false,
+    type: 'string'
+  })
+  .help()
+  .alias('help', 'h').argv
 
 async function main () {
   const files = await fs.readdir(path.join(process.cwd(), 'packages'))
 
   const reports = files
-    .filter(dir => fs.existsSync(path.join('packages', dir, 'coverage', 'lcov.info')))
+    .filter(dir => fs.existsSync(path.join('packages', dir, argv.coverageDir, 'lcov.info')))
     .map(dir => {
       return {
         type: 'lcov',
-        reportFile: path.join('packages', dir, 'coverage', 'lcov.info'),
+        reportFile: path.join('packages', dir, argv.coverageDir, 'lcov.info'),
         workingDirectory: path.join('packages', dir)
       }
     })
